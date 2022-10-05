763796a5fd3449fdcafba2a20287064a535424a9
Vincent Turmine <vincent.turmine@gmail.com>
2022-10-05 16:43:13 +0200

    add key figures

--- barchart.js
+++ barchart.js
@@ -1,8 +1,22 @@
+const arr = ['a', 'b', 'a', 'a', 'c', 'c'];
+
+const count = {};
+
+for (const element of arr) {
+  if (count[element]) {
+    count[element] += 1;
+  } else {
+    count[element] = 1;
+  }
+}
+
+
+
 d3.select("#item-1").append("span")
-    .text("3");
+    .text(count['a']);
 
 d3.select("#item-2").append("span")
-        .text("11");
+        .text(count['b']);
 
 d3.select("#item-3").append("span")
-        .text("0");
+        .text(count['c']);
