dac06f171b67b26b7fd658317ace408179fa6e6d
Vincent Turmine <vincent.turmine@gmail.com>
2022-10-27 14:47:54 +0200

    update

--- script.js
+++ script.js
@@ -120,7 +120,7 @@ let countriesISO3Arr = [];
 function initiateMap() {
     width = document.getElementById("mainOfIframe").offsetWidth; //viewportWidth;
     height = (isMobile) ? 350 : 450;
-    var mapScale = (isMobile) ? width / 5.2 : width / 5;
+    var mapScale = (isMobile) ? width / 5.2 : width / 5.2;
     var mapCenter = (isMobile) ? [12, 25] : [10, 25];
     projection = d3.geoMercator()
         .center(mapCenter)
